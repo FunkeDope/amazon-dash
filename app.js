1df7848aed7ad1b689d456bc6d25ac3052204437
funkedope <kyle5077@gmail.com>
2015-09-09 22:58:58 -0400

    added keyboard emulation and a dev flag for dev features

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 //either make your own credential file or just comment this out and fill in your api keys where needed
 var creds = require('./creds');
 
-var DASH_MAC = '74:c2:46:02:1c:72',
-    DASH_IP = '10.1.10.85';
+const DASH_MAC = '74:c2:46:02:1c:72',
+    DASH_IP = '10.1.10.85',
+    INTERFACE = 'wlan0', //'eth0';
+    DEV_MODE = true; //SPACEBAR emulates a button press
 
 var pcap = require("pcap"),
-    pcap_session = pcap.createSession("eth0", "arp"),
+    pcap_session = pcap.createSession(INTERFACE, "arp"),
     timeStamp = 0;
 
 console.log("Listening on " + pcap_session.device_name);
@@ -18,14 +20,14 @@ pcap_session.on('packet', function (raw_packet) {
     'use strict';
     var packet = pcap.decode.packet(raw_packet),
         device = packet.payload.payload.sender_ha.toString().toLowerCase();
-    
+
     /*console.log('--------- NEW ARP -------------');
     console.log('sender_ha: ' + packet.payload.payload.sender_ha);
     console.log('sender_pa: ' + packet.payload.payload.sender_pa);
     console.log('target_ha: ' + packet.payload.payload.target_ha);
     console.log('target_pa: ' + packet.payload.payload.target_pa);
     console.log('----------------------');*/
-    
+
     if(device === DASH_MAC) {
         var now = Date.now();
         if((now - timeStamp) >= 30000) { //multiple arp's are sent out within a few seconds of each other. we only need one.
@@ -108,4 +110,24 @@ function pushOver() {
             console.log(res);
         }
     });
+}
+
+
+// ----- for dev: space bar = dash press --------- //
+if(DEV_MODE === true) {
+    var keypress = require('keypress');
+    // make `process.stdin` begin emitting "keypress" events
+    keypress(process.stdin);
+    // listen for the "keypress" event
+    process.stdin.on('keypress', function (ch, key) {
+        if(key && key.ctrl && key.name == 'c') {
+            process.exit();
+         }
+         else if(key && key.name === 'space') {
+             dashButtonPress();
+         }
+    });
+
+    process.stdin.setRawMode(true);
+    //process.stdin.resume();
 }
